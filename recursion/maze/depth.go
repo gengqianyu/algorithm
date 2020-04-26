31d1146d2038c9aa3c901b1cbd96b6b0efbbc675
unknown <18612314865@163.com>
2020-04-26 20:02:03 +0800

    add mem

--- recursion/maze/depth.go
+++ recursion/maze/depth.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -19,7 +20,14 @@ func main() {
 	//起始位置
 	s := point{0, 0}
 	e := point{len(m) - 1, len(m[0]) - 1}
-	DepthWalk(m, s, e, &p)
+	p, err = DepthWalk(&m, s, e, &p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%v", p)
+	for _, e := range p {
+		fmt.Println(e)
+	}
 }
 
 func ReadMaze(src string) ([][]int, error) {
@@ -65,60 +73,57 @@ func (p point) move(r point) point {
 }
 
 // 定位移动点在地图中的位置。
-func (p point) position(m [][]int) (int, bool) {
+func (p point) position(m *[][]int) (int, bool) {
 	//判断上下是否越界
-	if p.i < 0 || p.i >= len(m) {
+	if p.i < 0 || p.i >= len(*m) {
 		return 0, false
 	}
 	//判断左右是否越界
-	if p.j < 0 || p.j >= len(m[p.i]) {
+	if p.j < 0 || p.j >= len((*m)[p.i]) {
 		return 0, false
 	}
-	return m[p.i][p.j], true
+	return (*m)[p.i][p.j], true
 }
 
 // 深度优先算法
 // m 地图 s开始点 e终点
-func DepthWalk(m [][]int, s point, e point, p *[]point) ([]point, error) {
+func DepthWalk(m *[][]int, s point, e point, p *[]point) ([]point, error) {
 	if s == e {
 		return *p, nil
 	}
-
-	if  {
-
+	if n, ok := Move(m, s, point{1, 0}); ok { //向下走
+		fmt.Println(1)
+		*p = append(*p, n)
+		return DepthWalk(m, n, e, p)
+	} else if n, ok := Move(m, s, point{-1, 0}); ok { //向右走
+		fmt.Println(2)
+		*p = append(*p, n)
+		return DepthWalk(m, n, e, p)
+	} else if n, ok := Move(m, s, point{-1, 0}); ok { //向上走
+		fmt.Println(3)
+		*p = append(*p, n)
+		return DepthWalk(m, n, e, p)
+	} else if n, ok := Move(m, s, point{0, -1}); ok { //向左走
+		fmt.Println(4)
+		*p = append(*p, n)
+		return DepthWalk(m, n, e, p)
+	} else {
+		return nil, nil
 	}
-	//向下走
-	n := s.move(point{1, 0})
-	v, ok := n.position(m)
-	if ok && v != 1 {
-		DepthWalk(m, n, e, p)
-	}
-	//向左走
-	n = s.move(point{0, -1})
-	v, ok = n.position(m)
-	if ok && v != 1 {
-		DepthWalk(m, n, e, p)
-	}
-	//向上走
-	n = s.move(point{-1, 0})
-	v, ok = n.position(m)
-	if ok && v != 1 {
-		DepthWalk(m, n, e, p)
-	}
-	//向右走
-	n = s.move(point{0, 1})
-	v, ok = n.position(m)
-	if ok && v != 1 {
-		DepthWalk(m, n, e, p)
-	}
-
 }
 
-func Move()  {
-	//向下走
-	n := s.move(point{1, 0})
+// 迷宫移动
+// m 地图 s是起始点
+func Move(m *[][]int, s, p point) (point, bool) {
+	//点移动
+	n := s.move(p)
 	v, ok := n.position(m)
-	if ok && v != 1 {
-		DepthWalk(m, n, e, p)
+	if ok && v == 0 {
+		(*m)[n.i][n.j] = 2
+		return n, true
 	}
+	if ok && v == 1 {
+		return point{}, false
+	}
+	return point{}, false
 }
--- recursion/recursion.go
+++ recursion/recursion.go
@@ -70,6 +70,7 @@ func factorial(n int) int {
 func fib(n int) int {
 	if n == 0 {
 		return 0
+
 	}
 	if n < 3 {
 		return 1
