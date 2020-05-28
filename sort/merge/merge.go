efdb298cc72e0ff86e27974a723d2b260156b485
unknown <18612314865@163.com>
2020-05-28 21:37:35 +0800

    add merge and test

---
+++ sort/merge/merge.go
@@ -0,0 +1,76 @@
+//归并排序(merge-sort)是利用归并的思想实现的排序方法，
+//该算法采用经典的分治策略，(分治方法将问题分成一些小的问题然后递归求解，而治的阶段则将分的阶段得到的各答案，"合并"在一起，即分而治之)
+package merge
+
+//多路channel 两两合并 如果参数是n个连续一样的值，可以用这种 参数... 的形式，接收到一个切片中
+func MergeN(ins ...<-chan int) <-chan int {
+	if len(ins) == 1 {
+		return ins[0]
+	}
+	m := len(ins) / 2
+	return Merge(MergeN(ins[:m]...), MergeN(ins[m:]...))
+	//merge(mergen(c1,c2,c3,c4),mergen(c5,c6,c7,c8))
+	//merge(merge(mergen(c1,c2),mergen(c3,c4)),merge(mergen(c5,c6),mergen(c7,c8)))
+	//merge(merge(merge(mergen(c1),mergen(c2)),merge(mergen(c3),mergen(c4))),merge(merge(mergen(c5),mergen(c6)),merge(mergen(c7),mergen(c8))))
+	//mergen(c1)=>c1
+	//merge(merge(merge(c1,c2),merge(c3,c4)),merge(merge(c5,c6),merge(c7,c8)))
+	//merge(c1,c2)=>out1
+	//merge(merge(out1,out2),merge(out3,out4))
+	//merge(out1,out2)=>out12
+	//merge(out12,out34)
+	//merge(out12,out34)=>out1234
+}
+
+func Merge(in1, in2 <-chan int) <-chan int {
+	out := make(chan int)
+
+	go func() {
+		//分别从两个channel里接一个数据
+		v1, ok1 := <-in1
+		v2, ok2 := <-in2
+		//如果能接到数据就循环
+		for ok1 || ok2 {
+			//如果in2中没数据，或者in1中有数据，同时v1<v2 就将V1放入 out
+			if !ok2 || (ok1 && v1 <= v2) {
+				out <- v1
+				//接下一组数
+				v1, ok1 = <-in1
+			} else {
+				out <- v2
+				v2, ok2 = <-in2
+			}
+		}
+		//如果没有数据了发送了就关闭通道
+		close(out)
+
+	}()
+	return out
+}
+
+func Sort(s []int) []int {
+	//分组
+	var ins []<-chan int
+	//这里需注意如果用make定义,下面循环得用ins[i]赋值，如果再用append相当于增加了ins的长度
+	//ins:=make([]<-chan int,len(s))
+	for _, e := range s {
+		ins = append(ins, MakeIn(e))
+	}
+	//合并
+	out := MergeN(ins...)
+
+	i := 0
+	for e := range out {
+		s[i] = e
+		i++
+	}
+	return s
+}
+
+func MakeIn(i int) <-chan int {
+	out := make(chan int)
+	go func(i int) {
+		out <- i
+		close(out)
+	}(i)
+	return out
+}
---
+++ sort/merge/merge_test.go
@@ -0,0 +1,26 @@
+package merge
+
+import (
+	"log"
+	"testing"
+)
+
+func TestSort(t *testing.T) {
+	s := [][]int{
+		{3, 5, 7, 8, 9, 4, 1, 2, 6, 0},
+		//{10, 17, 11, 13, 12, 14, 15, 16, 18, 20, 19},
+	}
+	var r []int
+	for _, e := range s {
+		r = append(r, Sort(e)...)
+	}
+	for _, e := range r {
+		log.Println(e)
+	}
+
+	for i := 0; i < len(r); i++ {
+		if r[i] != i {
+			t.Errorf("expected %d,actual %d", i, r[i])
+		}
+	}
+}
