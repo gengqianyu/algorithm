94051bca9cffcba232d1e5c0d6f1b919e31c5c35
gengqianyu <gengqianyu@gmail.com>
2020-09-15 15:00:48 +0800

    edit gomap comment

--- graph/gomap.go
+++ graph/gomap.go
@@ -53,8 +53,9 @@ func (m *GoMap) GetVertices() []string {
 	return m.vertices
 }
 
-//得到第一个邻接节点的索引
+//得到第一个邻接节点的索引，//遍历矩阵一行中的所有节点，找到邻接
 func (m *GoMap) FirstAdjacentIndex(i int) int {
+	//以i为起点在矩阵中找第一个关系邻接节点
 	for j := 0; j < len(m.vertices); j++ {
 		if m.edges[i][j] == 1 {
 			return j
@@ -64,8 +65,10 @@ func (m *GoMap) FirstAdjacentIndex(i int) int {
 }
 
 //根据前一个邻接节点的索引，来获取下一个邻接顶点
-func (m *GoMap) NextAdjacentIndex(i, b int) int {
-	for j := b + 1; j < len(m.vertices); j++ {
+//i为起始节点，c为i的当前邻接节点
+func (m *GoMap) NextAdjacentIndex(i, c int) int {
+	//遍历矩阵一行中的所有节点，找到邻接
+	for j := c + 1; j < len(m.vertices); j++ {
 		if m.edges[i][j] == 1 {
 			return j
 		}
@@ -128,13 +131,13 @@ func (m *GoMap) bfs(f func(string), i int) {
 			w := m.FirstAdjacentIndex(index)
 			//如果邻接结点存在
 			for w != -1 {
-				//	判断是否访问过
+				//	判断是否访问过，未访问就处理
 				if m.marks[w] == false {
 					f(m.vertices[w])
-					m.marks[w] = true
+					m.marks[w] = true //标记已访问
 					q.Push(w)
 				}
-				//以index为当前结点，去找下一个邻接节点
+				//以index为当前结点，以index的邻接节点w，去找下一个邻接节点，如果存在邻接就处理，一直在矩阵的一行中找不到邻接节点为止
 				w = m.NextAdjacentIndex(index, w)
 			}
 		}
